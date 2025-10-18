f3e5613554ca5ce45eb49de0f0d8a322621baa9a
vodoleikin <vodoleikin@yandex.ru>
2025-10-18 10:27:42 +0300

    18.10.2025. 10.25. v5. All tests passed. Correction of comments in exceptions.py.

--- exceptions.py
+++ exceptions.py
@@ -1,23 +1,18 @@
 class UnsuccessfulHTTPStatusCodeError(Exception):
     """Статус-код ответа сервера не равен 200."""
-    pass
 
 
 class RequestExceptError(Exception):
     """Ошибка запроса."""
-    pass
 
 
 class EmptyDataError(Exception):
     """Пустой словарь или список."""
-    pass
 
 
 class UnknownStatusError(Exception):
     """Неизвестный статус проверки работы."""
-    pass
 
 
 class TelegramMsgError(Exception):
     """Ошибка отправки сообщения в Telegram."""
-    pass
